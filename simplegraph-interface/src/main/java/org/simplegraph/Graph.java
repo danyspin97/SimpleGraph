597b51641defef89e934e7ba2d6156a00b1c4a94
Danilo Spinella <danyspin97@protonmail.com>
2018-10-03 18:23:18 +0200

    Add method countVertices in Graph interface

--- simplegraph-interface/src/main/java/org/simplegraph/Graph.java
+++ simplegraph-interface/src/main/java/org/simplegraph/Graph.java
@@ -19,6 +19,8 @@ public interface Graph<V> {
 
     public LinkedList<V> getNeighbors(V vertex);
 
+    public int countVertices();
+
     public int countNeighbors(V vertex);
 
     public int countEdges();
